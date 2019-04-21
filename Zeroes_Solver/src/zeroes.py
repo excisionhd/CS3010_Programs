9aa40838c4f25e7adef580c279b88d087ebda7d3
Andrew <excisionhd1@gmail.com>
2019-04-21 02:08:08 -0700

    [UPDATE] Complete bisection method

--- Zeroes_Solver/src/zeroes.py
+++ Zeroes_Solver/src/zeroes.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import argparse
 
 class Polynomial():
     def __init__(self, degree, coeff):
@@ -30,3 +31,34 @@ def ImportPolynomial(path):
         coeff = [int(i) for i in file.readline().split(' ')]
 
     return Polynomial(degree, coeff)
+
+def Bisection(polynomial, max_iter, p1, p2):
+    fx1 = polynomial.evaluate(p1)
+    fx2 = polynomial.evaluate(p2)
+
+    if(fx1 * fx2 > 0):
+        return
+
+    x1 = p1
+    x2 = p2
+
+    for i in range(max_iter):
+        mid = float((x1 + x2)/2)
+        fmid = polynomial.evaluate(mid)
+        print(fmid)
+        if fmid == 0:
+            return mid
+        if fx1 * fmid < 0:
+            x2 = mid  
+        else:
+            x1 = mid
+
+    return
+
+
+
+def main():
+    poly = ImportPolynomial("sys1.pol")
+    print(Bisection(poly, 100, -5, 5))
+
+main()
