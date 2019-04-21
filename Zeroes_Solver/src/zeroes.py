bae4345dc0db46e532a0cfab8671b2d06c0dcbf5
Andrew <excisionhd1@gmail.com>
2019-04-21 01:24:40 -0700

    [UPDATE] Complete import polynomial and begin defining classes

--- Zeroes_Solver/src/zeroes.py
+++ Zeroes_Solver/src/zeroes.py
@@ -1,1 +1,27 @@
-print("Begin")
+import sys
+import os
+
+class Polynomial():
+    def __init__(self, degree, coeff):
+        self.degree = degree
+        self.coeff = coeff
+
+    def evaluate(self, x):
+        result = 0
+        for i, num in enumerate(self.coeff):
+            result += num * (x ** (self.degree - i))
+        return result
+
+
+def ImportPolynomial(path):
+    if not os.path.isfile(path):
+        print("File path {} does not exist. Exiting...".format(path))
+        sys.exit()
+
+    with open(path, 'r') as file:
+        degree = int(file.readline())
+        coeff = [int(i) for i in file.readline().split(' ')]
+
+    return Polynomial(degree, coeff)
+
+print(ImportPolynomial("sys1.pol").evaluate(2))
