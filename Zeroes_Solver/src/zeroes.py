bf487f09360c4abd7dc1bcd7014f4f880e8ab2ee
Andrew <excisionhd1@gmail.com>
2019-04-21 16:33:23 -0700

    [UPDATE] Complete newton method, begin Secant

--- Zeroes_Solver/src/zeroes.py
+++ Zeroes_Solver/src/zeroes.py
@@ -45,20 +45,77 @@ def Bisection(polynomial, max_iter, p1, p2):
     for i in range(max_iter):
         mid = float((x1 + x2)/2)
         fmid = polynomial.evaluate(mid)
-        print(fmid)
         if fmid == 0:
             return mid
         if fx1 * fmid < 0:
             x2 = mid  
         else:
             x1 = mid
+    return
+
+def Newton(polynomial, max_iter, p1):
+    x1 = float(p1)
+    fx1 = polynomial.evaluate(x1)
+
+    for i in range(max_iter):
+        fd = polynomial.evalDeriv(x1)
+
+        if (fd == 0):
+            print("Minimum/maximum reached: ({} , {})".format(x1,fx1))
+            return x1
+        d = fx1 / fd
+        x1 = x1 - d
+        fx1 = polynomial.evaluate(x1)
+
+        if (d == 0):
+            print("Converged after {} iterations...".format(i+1))
+            return x1
 
     return
+
+def Secant(polynomial, max_iter, p1, p2):
+    x1 = p1
+    x2 = p2
+
+    fx1 = polynomial.evaluate(p1)
+    fx2 = polynomial.evaluate(p2)
+    """
+    fa := call f(a)
+  fb := call f(b)
+
+  if |fa| > |fb| then
+    swap(a, b)
+    swap(fa, fb)
+  end if
+
+  for it <- 1 to maxIter
+    if |fa| > |fb| then
+      swap(a, b)
+      swap(fa, fb)
+    end if
+
+    d := (b - a) / (fb - fa)
+    b := a
+    fb := fa
+    d := d * fa
+
+    if |d| < eps then
+      print "Algorithm has converged after #{it} iterations!"
+      return a
+    end if
+
+    a := a - d
+    fa := call f(a)
+  end for
+
+    print "Maximum number of iterations reached!"
+    return a
+    """
 
 
 
 def main():
     poly = ImportPolynomial("sys1.pol")
-    print(Bisection(poly, 100, -5, 5))
+    print("X = {}".format(Newton(poly, 10000, -1)))
 
 main()
