8ee63722b27626c329b8bc712ed3f55d55db7600
Andrew <excisionhd1@gmail.com>
2019-05-06 20:29:17 -0700

    [UPDATE] main

--- Polynomial_Interpolation/src/Main.java
+++ Polynomial_Interpolation/src/Main.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 class Point{
-    float x,y;
+    double x,y;
 
-    public Point(float x,float y){
+    public Point(double x,double y){
         this.x = x;
         this.y = y;
     }
@@ -31,8 +31,8 @@ public class Main {
             int min = Math.min(xs.length, xs.length);
 
             for(int i = 0 ;i < min - 1; i++){
-                float x = Float.parseFloat(xs[i]);
-                float y = Float.parseFloat(ys[i]);
+                double x = Float.parseFloat(xs[i]);
+                double y = Float.parseFloat(ys[i]);
                 points.add(new Point(x,y));
             }
         }
@@ -43,20 +43,10 @@ public class Main {
         return points;
 
     }
-    public static void main(String[] args) {
 
-        ArrayList<Point> pts = ReadData("src/sys1.pts");
-        float[] cs = new float[pts.size()];
-        Coeff(pts, cs);
-        float result = EvalNewton(pts, cs, 2);
-        System.out.println(Arrays.toString(cs));
-        System.out.println(result);
+    public static void Coeff(ArrayList<Point> points, double[] cs){
 
-    }
-
-    public static void Coeff(ArrayList<Point> points, float[] cs){
-
-        int n = points.size() - 1;
+        int n = points.size();
 
         for(int i = 0; i < n; i++){
             cs[i] = points.get(i).y;
@@ -70,36 +60,50 @@ public class Main {
 
     }
 
-    public static float EvalNewton(ArrayList<Point> points, float[] cs, float z){
-        float result = cs[points.size() -1];
+/*    function Coeff(xs : vector(n+1), ys : vector(n+1), cs : vector(n+1))
+            for i <- 0 to n
+    cs[i] := ys[i]
+    end for
 
-        for(int i = points.size() - 2; i>=0; i--){
+            for j <- 1 to n
+    for i <- n down to j
+    cs[i] := (cs[i] - cs[i-1]) / (xs[i] - x[i-j])
+    end for
+    end for
+    end function
+
+    function EvalNewton(xs : vector(n+1), cs : vector(n+1), z : float)
+    result := cs[n]
+
+            for i <- (n-1) down to 0
+    result := result * (z - xs[i]) + cs[i]
+    end for
+
+            return result
+    end function*/
+
+    public static double EvalNewton(ArrayList<Point> points, double[] cs, double z){
+        int n = points.size();
+
+        double result = cs[n-1];
+
+        for(int i = n - 2; i>=0; i--){
             result = result * (z - points.get(i).x) + cs[i];
         }
 
         return result;
     }
+    public static void main(String[] args) {
+
+        ArrayList<Point> pts = ReadData(currentDir + "/src/sys1.pts");
+        double[] cs = new double[pts.size()];
+        Coeff(pts, cs);
+
+        double result = EvalNewton(pts, cs, 10.162);
+        System.out.println(Arrays.toString(cs));
+        System.out.println(result);
+
+    }
+
+
 }
-
-/*    function Coeff(xs : vector(n+1), ys : vector(n+1), cs : vector(n+1))
-        for i <- 0 to n
-        cs[i] := ys[i]
-        end for
-
-        for j <- 1 to n
-        for i <- n down to j
-        cs[i] := (cs[i] - cs[i-1]) / (xs[i] - x[i-j])
-        end for
-        end for
-        end function
-
-        function EvalNewton(xs : vector(n+1), cs : vector(n+1), z : float)
-        result := cs[n]
-
-        for i <- (n-1) down to 0
-        result := result * (z - xs[i]) + cs[i]
-        end for
-
-        return result
-        end function
-        */
